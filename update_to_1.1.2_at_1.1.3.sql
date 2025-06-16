8dbddd9ca304aa6937ee4f72123b9696cbf1076f
cristianhoyosramirez <112783106+cristianhoyosramirez@users.noreply.github.com>
2025-06-16 15:58:12 -0500

    2025-06-16

--- update_to_1.1.2_at_1.1.3.sql
+++
@@ -1,9 +0,0 @@
-﻿
-INSERT INTO public.concepto_kardex (id, idoperacion, nombre, imagen) VALUES (31, 1, 'CONTEO MANUAL', 'A');
-
-INSERT INTO public.tipo_empresa (id, nombre)
-SELECT 3, 'Comercio al por menor con edicion de precios'
-WHERE NOT EXISTS (
-    SELECT 1 FROM public.tipo_empresa WHERE id = 3
-);
-update configuracion_pedido set version = '1.1.3 23 de mayo de 2025';
